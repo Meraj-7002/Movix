3160aecbbc69f00f7b52431c9eb48ab3b60ecdbc
Meraj-7002 <124896686+Meraj-7002@users.noreply.github.com>
2024-01-11 14:31:12 +0530

    Update useFetch.jsx
    Implemented Caching Mechanism.

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,25 +1,52 @@
 import { useEffect, useState } from "react";
 import { fetchDataFromApi } from "../utils/api";
-const useFetch = (url) => {
-    const [data, setData] = useState(null);
+
+const useFetch = (url, sessionTimeout = 30) => {
+    const isDataExpired = (cachedData) => {
+        const { timestamp, data } = cachedData;
+        const now = Date.now();
+        return now - timestamp > sessionTimeout * 1000 || !data;
+    };
+
+    const [data, setData] = useState(() => {
+        const cachedData = sessionStorage.getItem(url);
+        if (cachedData) {
+            const parsedData = JSON.parse(cachedData);
+            if (!isDataExpired(parsedData)) {
+                console.log("Data retrieved from cache:", parsedData.data);
+                return parsedData.data;
+            } else {
+                // Clear session storage if data is expired
+                console.log("Sessrion Storage is Empty")
+                sessionStorage.removeItem(url);
+            }
+        }
+        return null;
+    });
+
     const [loading, setLoading] = useState(null);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        setLoading("loading...");
-        setData(null);
-        setError(null);
-
-        fetchDataFromApi(url)
-            .then((res) => {
-                setLoading(false);
-                setData(res);
-            })
-            .catch((err) => {
-                setLoading(false);
-                setError("Something went wrong!");
-            });
-    }, [url]);
+        if (!data) {
+            setLoading("loading...");
+            console.log("Fetching data from API...");
+            fetchDataFromApi(url)
+                .then((res) => {
+                    setLoading(false);
+                    setData(res);
+                    const timestamp = Date.now();
+                    const dataWithTimestamp = { timestamp, data: res };
+                    sessionStorage.setItem(url, JSON.stringify(dataWithTimestamp));
+                    console.log("Data fetched from API:", res);
+                })
+                .catch((err) => {
+                    setLoading(false);
+                    setError("Something went wrong!");
+                    console.error("Error fetching data:", err);
+                });
+        }
+    }, [url, data]);
 
     return { data, loading, error };
 };
