1591ccd9681342c56e51df2953b83df5d0d6372c
Meraj-7002 <124896686+Meraj-7002@users.noreply.github.com>
2024-01-11 14:35:51 +0530

    Update api.js
    Implemented Caching Mechanism.

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,15 +7,41 @@ const headers = {
     Authorization: "bearer " + TMDB_TOKEN,
 };
 
-export const fetchDataFromApi = async (url, params) => {
+const isDataExpired = (cachedData, sessionTimeout = 180) => {
+    const { timestamp, data } = cachedData;
+    const now = Date.now();
+    return now - timestamp > sessionTimeout * 1000 || !data;
+};
+
+export const fetchDataFromApi = async (url, params, sessionTimeout = 180) => {
     try {
+        const cachedData = sessionStorage.getItem(url);
+
+        if (cachedData) {
+            const parsedData = JSON.parse(cachedData);
+            if (!isDataExpired(parsedData, sessionTimeout)) {
+                console.log("Data retrieved from cache:", parsedData.data);
+                return parsedData.data;
+            } else {
+                // Clear session storage if data is expired
+                sessionStorage.removeItem(url);
+            }
+        }
+
         const { data } = await axios.get(BASE_URL + url, {
             headers,
             params,
         });
+
+        // Cache the fetched data
+        const timestamp = Date.now();
+        const dataWithTimestamp = { timestamp, data };
+        sessionStorage.setItem(url, JSON.stringify(dataWithTimestamp));
+        console.log("Data fetched from API:", data);
+
         return data;
     } catch (err) {
-        console.log(err);
-        return err;
+        console.error("Error fetching data:", err);
+        throw err;
     }
 };
